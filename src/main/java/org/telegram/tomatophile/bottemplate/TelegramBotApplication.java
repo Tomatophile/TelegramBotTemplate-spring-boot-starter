172a11bad87ab54bef23326fddbb5ea50a5aa3f4
Stroev Artyom <stroev8artyom@gmail.com>
2021-06-28 13:44:24 +0300

    same as @SpringBootApplication just declaration that ur app is bot

---
+++ src/main/java/org/telegram/tomatophile/bottemplate/TelegramBotApplication.java
@@ -0,0 +1,15 @@
+package org.telegram.tomatophile.bottemplate;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+@SpringBootApplication
+public @interface TelegramBotApplication {
+
+}
